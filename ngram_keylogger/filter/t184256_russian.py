003ecc7b2792d0aa11593656fd28b5509fc1c796
Alexander Sosedkin <monk@unboiled.info>
2021-05-16 16:54:35 +0200

    filters.t184256_russian: fixup

--- ngram_keylogger/filter/t184256_russian.py
+++ ngram_keylogger/filter/t184256_russian.py
@@ -1,11 +1,20 @@
 # Copyright (c) 2021, see AUTHORS. Licensed under GPLv3+, see LICENSE.
 
-
-QWERTY = "`QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>~qwertyuiop[]asdfghjkl;zxcvbmn,.'\""
-JCUKEN = "ЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЯЧСМИТЬБЮёйцукенгшщзхъфывапролджячсмитьбюэЭ"
+# ru-prefixed
+QWERTY = "~qwertyuiop[]asdfghjkl;zxcvbnm,.'"
+JCUKEN = 'ёйцукенгшщзхъфывапролджячсмитьбюэ'
+JCUKEN_SHIFT = 'ЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЯЧСМИТЬБЮЭ'
 RUSSIAN_MAP = {en: f'ru-{ru}' for en, ru in zip(QWERTY, JCUKEN)}
-RUSSIAN_MAP.update({en: ru for en, ru in zip('!@#$%^&*()/\\',
-                                             '!"№;%:?*().,')})
+RUSSIAN_MAP.update({f'shift-{en}': f'ru-{ru}' for en, ru in zip(QWERTY,
+                                                                JCUKEN_SHIFT)})
+
+# non-ru-prefixed
+QWERTY_ = '1234567890/'
+JCUKEN_ = '1234567890.'
+JCUKEN_SHIFT_ = '!"№;%:?*(),'
+RUSSIAN_MAP.update({en: ru for en, ru in zip(QWERTY_, JCUKEN_)})
+RUSSIAN_MAP.update({f'shift-{en}': ru for en, ru in zip(QWERTY_,
+                                                        JCUKEN_SHIFT_)})
 
 
 async def t184256_russian(gen):
