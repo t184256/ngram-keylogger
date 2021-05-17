ff01bafbea97d642727f9c4c65e263fa3c766ade
Alexander Sosedkin <monk@unboiled.info>
2021-05-17 21:56:31 +0200

    query: limit, contexts, keypresses-by-context

--- ngram_keylogger/app.py
+++ ngram_keylogger/app.py
@@ -19,7 +19,6 @@ import ngram_keylogger
 def cli(ctx, db):
     ctx.ensure_object(dict)
     ctx.obj['db'] = db
-    pass
 
 
 @cli.command()
@@ -33,19 +32,62 @@ def collect(ctx, device_path, config):
     ngram_keylogger.collect.collect(device_path, ctx.obj['db'], config)
 
 
+def align(smth, width):
+    s = str(smth)
+    if isinstance(smth, int):
+        return ' ' * (width - len(s)) + s
+    return s + ' ' * (width - len(s))
+
+
+def pprint(results):
+    if isinstance(results, int):
+        print(results)
+    elif not results:
+        print('nothing')
+    elif isinstance(results, tuple) and not isinstance(results[0], tuple):
+        for r in results:
+            print(results)
+    elif isinstance(results, tuple) and isinstance(results[0], tuple):
+        # assume a rectangle
+        max_widths = [max(len(str(results[row][col]))
+                          for row in range(len(results)))
+                      for col in range(len(results[0]))]
+        for row in results:
+            print('|'.join(align(x, max_widths[i]) for i, x in enumerate(row)))
+    else:
+        print('warning: unknown format')
+        print(results)
+
+
 @cli.group()
 @click.pass_context
-def query(ctx):
+@click.option('--contexts', default='*',
+              help='filter by contexts (e.g., `term:vi:*` or_`browser,other`)')
+@click.option('--limit', default=-1, type=int,
+              help='show a maximum of this many results')
+def query(ctx, contexts, limit):
     """
     Output various stats from the database.
     """
-    pass
+    ctx.ensure_object(dict)
+    ctx.obj['contexts'] = contexts
+    ctx.obj['limit'] = limit
+    ctx.obj['qargs'] = (ctx.obj['db'], ctx.obj['contexts'], ctx.obj['limit'])
 
 
 @query.command()
 @click.pass_context
 def keypresses_count(ctx):
     """
-    Print how many keypresses are recorded in the db.
+    Print how many keypresses are recorded.
     """
-    print(ngram_keylogger.query.keypresses_count(ctx.obj['db']))
+    pprint(ngram_keylogger.query.keypresses_count(*ctx.obj['qargs']))
+
+
+@query.command()
+@click.pass_context
+def keypresses_by_context(ctx):
+    """
+    Print how many keypresses are recorded, categorized by context.
+    """
+    pprint(ngram_keylogger.query.keypresses_by_context(*ctx.obj['qargs']))
--- ngram_keylogger/query.py
+++ ngram_keylogger/query.py
@@ -3,11 +3,35 @@
 import sqlite3
 
 
-def _query(db_path, *a, **kwa):
+def _query(db_path, limit, query, *parameters):
     with sqlite3.connect(f'file:{db_path}?mode=ro', uri=True) as con:
-        r = con.execute(*a, **kwa)
+        if limit != -1:
+            query += ' LIMIT ?'
+            parameters = parameters + (limit,)
+        r = con.execute(query, parameters)
         return tuple(r.fetchall())
 
 
-def keypresses_count(db_path):
-    return _query(db_path, 'SELECT SUM(count) FROM keys;')[0][0]
+def _contexts_to_sql(contexts):
+    contexts = contexts.replace('*', '%')
+    contexts = contexts.replace('?', '_')
+    contexts = [contexts] if ',' not in contexts else contexts.split(',')
+    if not contexts:
+        return 'true', []
+    return ' OR '.join(['context LIKE ?'] * len(contexts)), tuple(contexts)
+
+
+def keypresses_count(db_path, contexts, limit):
+    contexts_condition, contexts_values = _contexts_to_sql(contexts)
+    return _query(db_path, limit,
+                  f'SELECT SUM(count) FROM keys WHERE {contexts_condition}',
+                  *contexts_values)[0][0]
+
+
+def keypresses_by_context(db_path, contexts, limit):
+    contexts_condition, contexts_values = _contexts_to_sql(contexts)
+    return _query(db_path, limit,
+                  'SELECT SUM(count), context FROM keys '
+                  f'WHERE {contexts_condition} GROUP BY context '
+                  f'ORDER by SUM(count) DESC',
+                  *contexts_values)
